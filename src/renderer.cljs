d912a4b4b6522ca2e4bb7f62cc4eb5d3f457a9d2
a <a@as-Virtual-Machine.local>
2024-06-01 13:29:49 -0700

    Improve renderer initialization for better audio handling with async-await pattern.

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -5,7 +5,8 @@
 
 (defn init []
   (js/console.log "Initializing renderer")
-  (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true}))
-  (let [context (js/AudioContext. (clj->js {:sampleRate sample-rate}))]
-    (js-await [_ (.audioWorklet.addModule context "audio.js")]
-              (js/AudioWorkletNode. context "processor"))))
+  (js-await [media (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true}))]
+            (let [context (js/AudioContext. (clj->js {:sampleRate sample-rate}))]
+              (js-await [_ (.audioWorklet.addModule context "audio.js")]
+                        (let [processor (js/AudioWorkletNode. context "processor")]
+                          (.connect (.createMediaStreamSource context media) processor))))))
