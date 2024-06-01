7ccb55fed1abcf792f7a3ac78d4efe564c2af8fa
a <a@as-Virtual-Machine.local>
2024-06-01 13:26:33 -0700

    Add audio.js for AudioWorklet processing

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -1,4 +1,11 @@
-(ns renderer)
+(ns renderer
+  (:require [shadow.cljs.modern :refer [js-await]]))
+
+(def sample-rate 16000)
 
 (defn init []
-  (js/console.log "Initializing renderer"))
+  (js/console.log "Initializing renderer")
+  (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true}))
+  (let [context (js/AudioContext. (clj->js {:sampleRate sample-rate}))]
+    (js-await [_ (.audioWorklet.addModule context "audio.js")]
+              (js/AudioWorkletNode. context "processor"))))
