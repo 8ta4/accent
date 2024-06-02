aa04f0c9fdc7cc18f91ff0c4bc6203e750deb6dd
a <a@as-Virtual-Machine.local>
2024-06-01 17:19:22 -0700

    Add push function to handle audio data in renderer

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -15,6 +15,13 @@
 (def readable
   (atom (stream/Readable. (clj->js {:read (fn [])}))))
 
+(defn push [readable audio]
+  (->> audio
+       js/Float32Array.
+       .-buffer
+       js/Buffer.from
+       (.push readable)))
+
 (defn init []
   (js/console.log "Initializing renderer")
   (js-await [media (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true}))]
@@ -22,5 +29,6 @@
               (js-await [_ (.audioWorklet.addModule context "audio.js")]
                         (let [processor (js/AudioWorkletNode. context "processor")]
                           (.connect (.createMediaStreamSource context media) processor)
-                          (j/assoc-in! processor [:port :onmessage] (fn [message]))))))
+                          (j/assoc-in! processor [:port :onmessage] (fn [message]
+                                                                      (push @readable message.data)))))))
   (set! js/window.onkeydown handle))
