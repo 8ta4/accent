a1e6e0ee55071e053bc3dac00ecceae1779c1c1f
a <a@as-Virtual-Machine.local>
2024-06-01 17:01:04 -0700

    Add evaluate function for pronunciation evaluation

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -4,6 +4,13 @@
 
 (def sample-rate 16000)
 
+(defn evaluate []
+  (js/console.log "Evaluating pronunciation..."))
+
+(defn handle [event]
+  (when (= event.code "Space")
+    (evaluate)))
+
 (defn init []
   (js/console.log "Initializing renderer")
   (js-await [media (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true}))]
@@ -11,4 +18,5 @@
               (js-await [_ (.audioWorklet.addModule context "audio.js")]
                         (let [processor (js/AudioWorkletNode. context "processor")]
                           (.connect (.createMediaStreamSource context media) processor)
-                          (j/assoc-in! processor [:port :onmessage] (fn [message])))))))
+                          (j/assoc-in! processor [:port :onmessage] (fn [message]))))))
+  (.addEventListener js/window "keydown" handle))
