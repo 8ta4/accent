ae5ee219e4a88e54482a9f24cbf462b3b878df03
a <a@as-Virtual-Machine.local>
2024-07-16 23:42:16 -0700

    Write initial test for align function in alignment-test namespace

---
+++ src/alignment.cljs
@@ -0,0 +1,5 @@
+(ns alignment)
+
+(defn align
+  [x y]
+  [])
---
+++ test/alignment_test.cljs
@@ -0,0 +1,6 @@
+(ns alignment-test
+  (:require [alignment]
+            [clojure.test :refer [deftest is]]))
+
+(deftest alignment
+  (is (= [] (alignment/align [] []))))
