af47045bfb695f98f8765487fa58c4fdd4ef19e4
a <a@as-Virtual-Machine.local>
2024-07-17 02:31:47 -0700

    Add 'trace' function to alignment.cljs

--- src/alignment.cljs
+++ src/alignment.cljs
@@ -1,13 +1,21 @@
 (ns alignment)
 
+(defn trace
+  [alignment [i j] matrix]
+  (if (and (= i 0) (= j 0))
+    alignment
+    (recur alignment (:trace (nth (nth matrix i) j)) matrix)))
+
 (defn align
   [x y]
 ;; TODO: Implement alignment
-  (cons (cons {:score 0}
-              (map (fn [j]
-                     {:score 0 :trace [0 j]})
-                   (range (count x))))
-        (map (fn [i]
-               (cons {:score 0 :trace [i 0]} (repeat (count x) {})))
-             (range (count y))))
-  [])
+  (trace []
+         [(count y)
+          (count x)]
+         (cons (cons {:score 0}
+                     (map (fn [j]
+                            {:score 0 :trace [0 j]})
+                          (range (count x))))
+               (map (fn [i]
+                      (cons {:score 0 :trace [i 0]} (repeat (count x) {})))
+                    (range (count y))))))
