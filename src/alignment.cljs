a4f616e8cf8723e2d66d594a08663cda5a112f51
a <a@as-Virtual-Machine.local>
2024-07-17 01:27:59 -0700

    Refactor variable names in align function

--- src/alignment.cljs
+++ src/alignment.cljs
@@ -3,6 +3,6 @@
 (defn align
   [x y]
 ;; TODO: Implement alignment
-  (cons (cons {:score 0} (map (fn [x*] {:score 0 :trace [0 x*]}) (range (count x))))
-        (map (fn [y*] (cons {:score 0 :trace [y* 0]} (repeat (count x) {}))) (range (count y))))
+  (cons (cons {:score 0} (map (fn [j] {:score 0 :trace [0 j]}) (range (count x))))
+        (map (fn [i] (cons {:score 0 :trace [i 0]} (repeat (count x) {}))) (range (count y))))
   [])
