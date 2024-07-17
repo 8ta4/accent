cb174e105c08ccfe2687fb746b7009253b188121
a <a@as-Virtual-Machine.local>
2024-07-17 02:05:36 -0700

    Reformat align function to enhance code clarity

--- src/alignment.cljs
+++ src/alignment.cljs
@@ -3,6 +3,11 @@
 (defn align
   [x y]
 ;; TODO: Implement alignment
-  (cons (cons {:score 0} (map (fn [j] {:score 0 :trace [0 j]}) (range (count x))))
-        (map (fn [i] (cons {:score 0 :trace [i 0]} (repeat (count x) {}))) (range (count y))))
+  (cons (cons {:score 0}
+              (map (fn [j]
+                     {:score 0 :trace [0 j]})
+                   (range (count x))))
+        (map (fn [i]
+               (cons {:score 0 :trace [i 0]} (repeat (count x) {})))
+             (range (count y))))
   [])
